04fb1b61babe4d26c981d16ea91a2d4c0df187fb
18955515189 <18955515189@163.com>
2018-05-27 13:56:21 +0800

    zookeeper JAVA 代码实现 Create by David 2018-05-27

--- src/main/java/com/zookeeper/SimpleZkClient.java
+++ src/main/java/com/zookeeper/SimpleZkClient.java
@@ -1,6 +1,7 @@
 package com.zookeeper;
 
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -35,13 +36,13 @@ public class SimpleZkClient {
                 System.out.println( watchedEvent.getType() +"-  -  -  -  -  - "+ watchedEvent.getPath() );
                 System.out.println( watchedEvent.getState() );
 
-                /*try {
+                try {
                     zkClient.getData( "/test1",true,null );
                 } catch (KeeperException e) {
                     e.printStackTrace();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }*/
+                }
 
             }
         });
@@ -54,7 +55,7 @@ public class SimpleZkClient {
     @Test
     public void testCreate(){
         try {
-            String node = zkClient.create( "/mygirls","helloworld".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT_SEQUENTIAL);
+            String node = zkClient.create( "/mygirls","helloworld".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
             System.out.println( "创建成功 :"+ node );
             zkClient.close();
         } catch (KeeperException e) {
@@ -64,4 +65,68 @@ public class SimpleZkClient {
         }
     }
 
+    /**
+     * 测试获取节点数据 c测试watcher 工作
+     */
+    @Test
+    public void testGet(){
+        try {
+            String value = String.valueOf(zkClient.getData( "/test1",true,null ));
+            System.out.println( "获取test1数据成功 :"+ value );
+            Thread.sleep( Integer.MAX_VALUE );
+           // zkClient.close();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 测试删除节点
+     */
+    @Test
+    public void testDelete(){
+        try {
+             zkClient.delete( "/mygirl",-1 );
+            System.out.println(" 删除成功 ");
+             zkClient.close();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 测试修改节点数据
+     */
+    @Test
+    public void testUpdate(){
+        try {
+            zkClient.setData( "/mygirls","aaaaa".getBytes(),-1 );
+            System.out.println(" 修改成功 ");
+            zkClient.close();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 测试判断节点是否存在
+     */
+    @Test
+    public void testExists(){
+        try {
+            Stat stat = zkClient.exists( "/mygirl",true );
+            System.out.println( stat==null ? "不存在":"存在" );
+            zkClient.close();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
